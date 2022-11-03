7953c7ca96a4842f7eedb5de6ead2e639d470c75
warwick <warwick.nortier@gmail.com>
2022-11-03 14:36:55 +0200

    added styled components

---
+++ src/components/styles/card/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles, theme } from './styles';
+import { ThemeProvider } from 'styled-components';
+import { Content, Title } from './components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Content>
+        <Title>Hello World</Title>
+      </Content>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
