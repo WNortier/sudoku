7cf0f653e6c69e2d394fd8aa78ea2c1f24baa768
warwick <warwick.nortier@gmail.com>
2022-11-07 12:43:12 +0200

    added creategrid method to grid reducer

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,25 +1,24 @@
-import { decrement, increment } from '../redux/reducers/counter-slice'
+import { create_grid } from '../redux/reducers/counter-slice'
 import { useAppSelector, useAppDispatch } from '../hooks/redux'
 
 export function Counter() {
-    const count = useAppSelector((state) => state.counter.value)
+    // const count = useAppSelector((state) => state.counter.value)
     const dispatch = useAppDispatch()
 
     return (
         <div>
             <div>
-                {count}
                 <button
                     aria-label="Increment value"
-                    onClick={() => dispatch(increment())}
+                    onClick={() => { }}
                 >
                     Increment
                 </button>
                 <button
                     aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}
+                    onClick={() => dispatch(create_grid())}
                 >
-                    Decrement
+                    Create Grid
                 </button>
             </div>
         </div>
